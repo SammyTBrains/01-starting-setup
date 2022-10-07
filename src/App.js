e8e7b0ca0417f0e4cf75a5f161b74d41a67d7465
SammyTBrains <josephsamuel034@gmail.com>
2022-10-07 20:38:38 +0100

    Moving Value Up

--- src/App.js
+++ src/App.js
@@ -24,9 +24,14 @@ function App() {
     },
   ];
 
+  const addExpenseDataHandler = (expenseData) => {
+    console.log("In App.js");
+    console.log(expenseData);
+  };
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpenseData={addExpenseDataHandler} />
       <Expenses data={expenses} />
     </div>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,10 +2,19 @@ import ExpenseForm from "./ExpenseForm";
 
 import "./NewExpense.css";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const submitExpenseFormHandler = (expenseData) => {
+    const expenseObj = {
+      ...expenseData,
+      id: Math.random().toString(),
+    };
+
+    props.onAddExpenseData(expenseObj);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSubmitExpenseForm={submitExpenseFormHandler} />
     </div>
   );
 };
