8b5f6b40ffb664828fce8601ae5e8cb61736ccf2
SammyTBrains <josephsamuel034@gmail.com>
2022-10-07 15:19:54 +0100

    change

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,5 +1,13 @@
 const ExpenseItems = () => {
-  return <h2>Expense Items!</h2>;
+  return (
+    <div>
+      <div>March 31, 2002</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseItems;
